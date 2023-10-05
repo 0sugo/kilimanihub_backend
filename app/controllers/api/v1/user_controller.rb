9f3b5841af05e3c01b5d8289f3316a6732bc5999
0sugo <josugo26@gmail.com>
2023-10-05 22:11:54 +0300

    Remove unwanted file

--- app/controllers/api/v1/user_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::UserController < ApplicationController
-  def index
-    @users = User.all
-    render json:{status:"Success",message:"Users fetched succesfully",data:@data},status: :ok
-  end
-
-  def show
-  end
-end
