67c847ea4ce13f0c5b4f9200f7bafa5695b666d1
0sugo <josugo26@gmail.com>
2023-10-05 22:14:43 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
-      get 'user/index'
-      get 'user/show'
-      resources :users, only: %i[index show] do
-        resources :posts, only: %i[index show new create destroy] do
-          resources :comments, only: %i[new create destroy]
-            resource :like, only: :create
-       end
+      get 'users/index'
+      get 'users/show'
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :new, :create, :destroy] do
+          resources :comments, only: [:new, :create, :destroy]
+          resource :like, only: :create
+        end
       end
     end
   end
