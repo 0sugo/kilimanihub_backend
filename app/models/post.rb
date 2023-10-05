ddf60c94c163df904ed34911240d8f169bec7f23
0sugo <josugo26@gmail.com>
2023-10-05 22:14:23 +0300

    Modify post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,11 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments, dependent: :destroy
   has_many :likes, class_name: 'Like', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  after_save :update_posts
+  # validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def display_comments
     comments.order(created_at: :desc).limit(5)
@@ -15,7 +13,4 @@ class Post < ApplicationRecord
 
   private
 
-  def update_posts
-    author.increment!(:posts_counter)
-  end
 end
