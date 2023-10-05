95ae62c1874c32bd3a9c87734802b2859b5ed0c2
0sugo <josugo26@gmail.com>
2023-10-05 14:54:41 +0300

    Modify database

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index show new create destroy] do
+          resources :comments, only: %i[new create destroy]
+            resource :like, only: :create
+       end
+      end
+    end
+  end
 end
